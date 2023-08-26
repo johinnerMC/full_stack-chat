37eb36e939e8b42a929ee35ab50c556be4d982a5
Johinner <johimadacor@live.com>
2023-08-26 19:29:27 -0400

    Conexion backend con MongoDB Atlas

---
+++ chat-backend/database/config.js
@@ -0,0 +1,22 @@
+const { default: mongoose } = require("mongoose")
+
+
+const dbConnection = async() => {
+    try {
+        
+        await mongoose.connect(process.env.DB_CNN_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("DB online");
+
+    } catch (error) {
+        console.log(error)
+        throw new Error("Error en la base de datos - vea logs")
+    }
+}
+
+module.exports = {
+    dbConnection
+}
